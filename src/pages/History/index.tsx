57d632c50b0f2d85039b1a8286adf4bd02671020
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2023-06-29 19:19:35 -0300

    finished History page layout

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,48 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="done">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="inProgress">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="stopped">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
